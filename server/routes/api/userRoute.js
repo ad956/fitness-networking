00c756adefc54428237a8ef87e858b89978e77f9
Anand Suthar <anandsuthar956@gmail.com>
2024-01-05 22:06:13 +0530

    added route for all users

--- server/routes/api/userRoute.js
+++ server/routes/api/userRoute.js
@@ -5,9 +5,8 @@ const userController = require("../../controllers/userController");
 const validateToken = require("../../middleware/validateTokenHandler");
 
 router.get("/", validateToken, userController.getUser);
-router.post("/", validateToken, userController.allUsers);
+router.get("/all", validateToken, userController.allUsers);
 router.post("/login", userController.login);
 router.post("/register", userController.registerUser);
-router.get("/credit", userController.registerUser);
 
 module.exports = router;
