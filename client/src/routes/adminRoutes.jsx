512f5db3de6584d00f8e9aad54a9db4ec26fc63e
Anand Suthar <anandsuthar956@gmail.com>
2024-01-12 16:50:49 +0530

    intial admin routes

---
+++ client/src/routes/adminRoutes.jsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from "react-router-dom";
+
+const AdminRoutes = () => (
+  <Routes>
+    <Route path="/" element={<p>Index page of ADMIN</p>} />
+  </Routes>
+);
+
+export default AdminRoutes;
