d18f0746e5363a0e4b0592daf581343e0d171a7c
Anand Suthar <anandsuthar956@gmail.com>
2024-01-12 16:51:04 +0530

    intial partner routes

---
+++ client/src/routes/partnerRoutes.jsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from "react-router-dom";
+
+const PartnerRoutes = () => (
+  <Routes>
+    <Route path="/" element={<p>Index page of Partner</p>} />
+  </Routes>
+);
+
+export default PartnerRoutes;
