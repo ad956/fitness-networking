7e6ed0689d71419a1561ad0a2793e7d6eeea70a6
Anand Suthar <anandsuthar956@gmail.com>
2024-01-12 16:33:11 +0530

    added AppRoutes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import AppRoutes from "./routes";
 
 function App() {
   return (
     <>
+      <AppRoutes />
       <div className="h-screen bg-slate-900">
         <h2 className="text-purple-500 ">Fitness Networking</h2>
       </div>
