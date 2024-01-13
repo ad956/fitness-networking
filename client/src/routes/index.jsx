ad209d32892b52ad0eb237c11e9a736944b5ab54
Anand Suthar <anandsuthar956@gmail.com>
2024-01-13 17:20:59 +0530

    temp changes

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -3,11 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import UserRoutes from "./userRoutes";
 import PartnerRoutes from "./partnerRoutes";
 import AdminRoutes from "./adminRoutes";
+import Login from "../features/auth/Login/";
+import Signup from "../features/auth/Signup";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<>Hey Boii</>} />
+      {/* <Route path="/" element={<>Hey Boii</>} /> */}
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/user/*" element={<UserRoutes />} />
       <Route path="/partner/*" element={<PartnerRoutes />} />
       <Route path="/admin/*" element={<AdminRoutes />} />
