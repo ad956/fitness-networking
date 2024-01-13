41ca4eb75dafc1b16945f45ffd2580bca9e7dd6f
Anand Suthar <anandsuthar956@gmail.com>
2024-01-13 17:22:01 +0530

    temp changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,16 @@ import AppRoutes from "./routes";
 function App() {
   return (
     <>
+      {/* <div className="h-screen bg-slate-900 text-white"> */}
+      {/* <h2 className="text-purple-500 ">Fitness Networking</h2>
+
+        <nav className="flex gap-5">
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </nav> */}
       <AppRoutes />
-      <div className="h-screen bg-slate-900">
-        <h2 className="text-purple-500 ">Fitness Networking</h2>
-      </div>
+      {/* </div> */}
     </>
   );
 }
