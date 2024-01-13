f7644411acc1ec27543d7bc3d5c6e2c93060aa93
Anand Suthar <anandsuthar956@gmail.com>
2024-01-13 17:22:40 +0530

    Theme provider added for mui

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "@fontsource/lato";
 import "./index.css";
+import { ThemeProvider } from "@mui/material";
 import { BrowserRouter as Router } from "react-router-dom";
+import { theme } from "./theme/theme.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Router>
   </React.StrictMode>
 );
