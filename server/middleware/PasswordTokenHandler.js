11bc87aa9aa7555af56f087589a3d4bfc3f33e33
Anand Suthar <anandsuthar956@gmail.com>
2024-01-14 15:51:42 +0530

    password secret validator

--- server/middleware/PasswordTokenHandler.js
+++ server/middleware/PasswordTokenHandler.js
@@ -1,8 +1,18 @@
 const asyncHandler = require("express-async-handler");
+const { DataTypes, Op } = require("sequelize");
+const { sequelize } = require("../config/dbConnection");
+const User = require("../models/userModel")(sequelize, DataTypes);
 
 const validatePasswordToken = asyncHandler(async (req, res, next) => {
   const token = req.params;
-  console.log(token);
+  console.log("ate punge riya ho");
+  const user = await User.findOne({ where: { otp: token } });
+  console.log("ne ate ate punge riya ho");
+
+  if (!user) {
+    res.status(401);
+    throw new Error("Token expired or isn't valid");
+  }
 });
 
 module.exports = validatePasswordToken;
