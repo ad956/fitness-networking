ba4c3399760321eb219932f18b7ec6c48bab56bc
Anand Suthar <anandsuthar956@gmail.com>
2024-01-14 15:43:25 +0530

    added forgot and reset password routes

--- server/routes/api/userRoute.js
+++ server/routes/api/userRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const userController = require("../../controllers/userController");
 const validateToken = require("../../middleware/validateTokenHandler");
+const validatePasswordToken = require("../../middleware/PasswordTokenHandler");
 
 router.get("/", validateToken, userController.getUser);
 router.get("/all", validateToken, userController.allUsers);
@@ -10,5 +11,10 @@ router.post("/login", userController.login);
 router.post("/register", userController.registerUser);
 router.post("/forgot-password", userController.forgetPassword);
 router.post("/reset-password", validateToken, userController.resetPassword);
+router.get(
+  "/reset-password/:token",
+  validatePasswordToken,
+  userController.setPassword
+);
 
 module.exports = router;
