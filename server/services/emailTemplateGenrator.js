da09d297f14e7178c1fc4c16943c0ce7326cdcd4
Anand Suthar <anandsuthar956@gmail.com>
2024-01-14 18:32:30 +0530

    template genrator service

---
+++ server/services/emailTemplateGenrator.js
@@ -0,0 +1,30 @@
+const Mailgen = require("mailgen");
+
+function mailTemplateGenrator(name, introMsg, instuctMsg, link, msg, outro) {
+  let MailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "Fitness Networking",
+      link: "http://localhost:5173/",
+    },
+  });
+
+  let response = {
+    body: {
+      name: name,
+      intro: introMsg,
+      action: {
+        instructions: instuctMsg,
+        button: {
+          color: "#DC4D2F",
+          text: msg,
+          link: link,
+        },
+      },
+      outro: outro,
+    },
+  };
+  return MailGenerator.generate(response);
+}
+
+module.exports = mailTemplateGenrator;
