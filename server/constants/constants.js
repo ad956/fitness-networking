c627d09311f9537ad0dd51a1696e80f36781888f
Anand Suthar <85487906+ad956@users.noreply.github.com>
2024-01-14 19:13:33 +0530

    Delete server/constants directory

--- server/constants/constants.js
+++
@@ -1,5 +0,0 @@
-const MAIL_FROM = "Fitness Networking fitness@gmail.com";
-const CLIENT_ERROR_URL = "http://localhost:5173/error";
-const CLIENT_URL = "http://localhost:5173/";
-
-module.exports = { MAIL_FROM, CLIENT_ERROR_URL, CLIENT_URL };
