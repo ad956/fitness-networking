60c6716ca09ac0c13b33222f9160a41b416dfea7
Anand Suthar <anandsuthar956@gmail.com>
2024-01-14 16:22:55 +0530

    resetpasswordToken Handler completed

--- server/middleware/PasswordTokenHandler.js
+++ server/middleware/PasswordTokenHandler.js
@@ -7,11 +7,19 @@ const validatePasswordToken = asyncHandler(async (req, res, next) => {
   const { token } = req.params;
   const user = await User.findOne({ where: { otp: token } });
 
-  console.log(token + " " + typeof token);
   if (!user) {
     res.status(401);
     throw new Error("Token expired or isn't valid");
   }
+
+  user.otp = null;
+  const result = await user.save();
+  if (!result) {
+    res.status(500);
+    throw new Error("Internal Server Error");
+  }
+  req.user = user;
+  next();
 });
 
 module.exports = validatePasswordToken;
