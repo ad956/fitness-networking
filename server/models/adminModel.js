907931ebde7c0733ce7c26160f47d14c53c0d181
Anand Suthar <anandsuthar956@gmail.com>
2024-01-15 15:35:14 +0530

    added admin model

---
+++ server/models/adminModel.js
@@ -0,0 +1,46 @@
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class Admin extends Model {
+    static associate(models) {
+      // Define associations here if needed
+    }
+  }
+
+  Admin.init(
+    {
+      admin_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      mobile: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      otp: DataTypes.STRING,
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      profilephoto: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Admin",
+      tableName: "admin",
+      timestamps: false,
+    }
+  );
+
+  return Admin;
+};
