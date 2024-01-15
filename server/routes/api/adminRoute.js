9117e2cddfe14a342366cdac1e73f3c78521e479
Anand Suthar <anandsuthar956@gmail.com>
2024-01-15 15:35:41 +0530

    added admin routes

--- server/routes/api/adminRoute.js
+++ server/routes/api/adminRoute.js
@@ -1,18 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-// Middleware for admin authentication
+const adminController = require("../../controllers/adminController");
+// validateToken
 
-// Route to manage users (e.g., view, edit, delete)
-router.get("/manage-users", (req, res) => {
-  // Display user management options
-});
-
-// Route to manage gyms (e.g., view, edit, delete)
-router.get("/manage-gyms", (req, res) => {
-  // Display gym management options
-});
-
-// Other routes for admin
+router.get("/", adminController.admin);
+router.get("/login", adminController.login);
 
 module.exports = router;
