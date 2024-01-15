b2565eadf1b61073b9d93cb0bc87141850088c18
Anand Suthar <anandsuthar956@gmail.com>
2024-01-15 15:46:22 +0530

    using /partner instead of owner

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const userRoutes = require("./api/userRoute");
-const ownerRoutes = require("./api/ownerRoute");
+const partnerRoutes = require("./api/partnerRoute");
 const adminRoutes = require("./api/adminRoute");
 
 router.use("/api/user", userRoutes);
-router.use("/api/owner", ownerRoutes);
+router.use("/api/partner", partnerRoutes);
 router.use("/api/admin", adminRoutes);
 
 module.exports = router;
