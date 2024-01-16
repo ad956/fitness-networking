bff383668607276a211378023c971c6430d4f3a0
Anand Suthar <anandsuthar956@gmail.com>
2024-01-16 23:12:30 +0530

    added reset password verification route

--- server/routes/api/partnerRoute.js
+++ server/routes/api/partnerRoute.js
@@ -10,5 +10,9 @@ router.post("/register", partnerController.register);
 router.get("/all", validateToken, userController.allUsers);
 router.post("/forgot-password", userController.forgetPassword); // at login
 router.post("/reset-password", validateToken, userController.resetPassword); // using profile
-
+router.get(
+  "/reset-password/:token",
+  validatePasswordToken,
+  userController.setPassword
+);
 module.exports = router;
