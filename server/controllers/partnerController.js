200153ba431fe50ab241b2cb4740484263a453f1
Anand Suthar <anandsuthar956@gmail.com>
2024-01-16 23:16:01 +0530

    added forget password in controller

--- server/controllers/partnerController.js
+++ server/controllers/partnerController.js
@@ -93,8 +93,69 @@ const getPartner = asyncHandler(async (req, res) => {
   return;
 });
 
+//forget-password
+const forgetPassword = asyncHandler(async (req, res, next) => {
+  const { email_mobile } = req.body;
+
+  if (!email_mobile) {
+    res.status(400).json({ msg: "Email or Mobile is mandatory" });
+    return;
+  }
+
+  const user = await User.findOne({
+    where: { [Op.or]: [{ email: email_mobile }, { mobile: email_mobile }] },
+  });
+
+  if (!user) {
+    res.status(400).json({ msg: "User doesn't exists" });
+    return;
+  }
+
+  const forgotPasswordSecret = genratedOTP;
+
+  user.otp = forgotPasswordSecret;
+
+  const otpChanged = await user.save();
+  if (!otpChanged) {
+    res.status(500).json({ msg: "OTP sending failed" });
+    return;
+  }
+
+  const forgotPasswordLink = `${constants.USER_URL}reset-password/${forgotPasswordSecret}`;
+
+  const introMsg =
+    "You have received this email because a forgot password request for your account was received.";
+  const instuctMsg = "Click the button below to reset your password:";
+  const link = forgotPasswordLink;
+  const msg = "Reset your password";
+  const outro =
+    "If you did not request a forgot password reset, no further action is required on your part.";
+
+  let mail = mailTemplateGenrator(
+    user.name,
+    introMsg,
+    instuctMsg,
+    link,
+    msg,
+    outro
+  );
+
+  // sending an email ...
+  let message = {
+    from: constants.MAIL_FROM,
+    to: user.email,
+    subject: "Reset Password",
+    html: mail,
+  };
+
+  const info = await sendEmail(message);
+  res.status(201).json(info);
+  return;
+});
+
 module.exports = {
   register,
   login,
   getPartner,
+  forgetPassword,
 };
