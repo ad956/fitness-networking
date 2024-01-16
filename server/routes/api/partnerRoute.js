5a2d37a7fce9225090d6bd1646e4a2cadab6dd19
Anand Suthar <anandsuthar956@gmail.com>
2024-01-16 23:10:08 +0530

    minor

--- server/routes/api/partnerRoute.js
+++ server/routes/api/partnerRoute.js
@@ -7,7 +7,6 @@ const validateToken = require("../../middleware/validateTokenHandler");
 router.get("/", validateToken, partnerController.getPartner);
 router.post("/login", partnerController.login);
 router.post("/register", partnerController.register);
-router.get("/", validateToken, userController.getUser);
 router.get("/all", validateToken, userController.allUsers);
 router.post("/login", userController.login);
 router.post("/register", userController.registerUser);
