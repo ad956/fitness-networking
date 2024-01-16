f1ca6cc5b408bf6a093fd461b8c1a465e33b559e
Anand Suthar <anandsuthar956@gmail.com>
2024-01-16 23:10:40 +0530

    added / all route

--- server/routes/api/partnerRoute.js
+++ server/routes/api/partnerRoute.js
@@ -8,7 +8,6 @@ router.get("/", validateToken, partnerController.getPartner);
 router.post("/login", partnerController.login);
 router.post("/register", partnerController.register);
 router.get("/all", validateToken, userController.allUsers);
-router.post("/login", userController.login);
 router.post("/register", userController.registerUser);
 router.post("/forgot-password", userController.forgetPassword); // at login
 router.post("/reset-password", validateToken, userController.resetPassword); // using profile
