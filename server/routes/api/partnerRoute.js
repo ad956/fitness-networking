253cb67ca6efbf2facd9c677ed95f3a6a39bac7b
Anand Suthar <anandsuthar956@gmail.com>
2024-01-16 23:09:52 +0530

    minor

--- server/routes/api/partnerRoute.js
+++ server/routes/api/partnerRoute.js
@@ -7,5 +7,15 @@ const validateToken = require("../../middleware/validateTokenHandler");
 router.get("/", validateToken, partnerController.getPartner);
 router.post("/login", partnerController.login);
 router.post("/register", partnerController.register);
-
+router.get("/", validateToken, userController.getUser);
+router.get("/all", validateToken, userController.allUsers);
+router.post("/login", userController.login);
+router.post("/register", userController.registerUser);
+router.post("/forgot-password", userController.forgetPassword); // at login
+router.post("/reset-password", validateToken, userController.resetPassword); // using profile
+router.get(
+  "/reset-password/:token",
+  validatePasswordToken,
+  userController.setPassword
+);
 module.exports = router;
