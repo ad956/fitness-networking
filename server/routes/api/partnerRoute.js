600a5f5c51e8df7eecb4963b53d613de122e6451
Anand Suthar <anandsuthar956@gmail.com>
2024-01-16 23:13:08 +0530

    removed all route

--- server/routes/api/partnerRoute.js
+++ server/routes/api/partnerRoute.js
@@ -7,7 +7,6 @@ const validateToken = require("../../middleware/validateTokenHandler");
 router.get("/", validateToken, partnerController.getPartner);
 router.post("/login", partnerController.login);
 router.post("/register", partnerController.register);
-router.get("/all", validateToken, userController.allUsers);
 router.post("/forgot-password", userController.forgetPassword); // at login
 router.post("/reset-password", validateToken, userController.resetPassword); // using profile
 router.get(
