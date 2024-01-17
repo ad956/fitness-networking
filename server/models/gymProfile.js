4bcc3e04299a2bed27c64b145ee8283631e50706
Anand Suthar <anandsuthar956@gmail.com>
2024-01-17 20:38:59 +0530

    gym profile model added

---
+++ server/models/gymProfile.js
@@ -0,0 +1,31 @@
+class GymProfile extends Model {
+  static associate(models) {
+    this.belongsTo(models.Gym, { foreignKey: "gym_id" });
+  }
+}
+
+GymProfile.init(
+  {
+    gym_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    total_credits: DataTypes.DECIMAL(10, 2),
+    upi_id: DataTypes.STRING,
+    credit_charge: DataTypes.DECIMAL(5, 2),
+    todays_credit: DataTypes.DECIMAL(10, 2),
+    profile_pic: DataTypes.STRING,
+    city: DataTypes.STRING,
+    pin_code: DataTypes.STRING,
+    state: DataTypes.STRING,
+    address: DataTypes.STRING,
+  },
+  {
+    sequelize,
+    modelName: "GymProfile",
+    tableName: "gym_profile",
+    timestamps: false,
+  }
+);
+
+module.exports = GymProfile;
