abd9d5a47d338d287bbebf24187aed3410353de1
Anand Suthar <anandsuthar956@gmail.com>
2024-01-17 20:43:02 +0530

    transactions model added

---
+++ server/models/transactionsModel.js
@@ -0,0 +1,34 @@
+class Transactions extends Model {
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: "user_id" });
+    this.belongsTo(models.Gym, { foreignKey: "gym_id" });
+  }
+}
+
+Transactions.init(
+  {
+    transaction_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+    },
+    gym_id: {
+      type: DataTypes.INTEGER,
+    },
+    transaction_type: DataTypes.ENUM("Purchase", "Usage"),
+    transaction_amount: DataTypes.DECIMAL(5, 2),
+    credit_purchased: DataTypes.DECIMAL(5, 2),
+    transaction_date: DataTypes.DATE,
+  },
+  {
+    sequelize,
+    modelName: "Transactions",
+    tableName: "transactions",
+    timestamps: false,
+  }
+);
+
+module.exports = Transactions;
