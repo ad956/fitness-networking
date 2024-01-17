9323c864470bc24d448ae650630ef1eda941b1dd
Anand Suthar <anandsuthar956@gmail.com>
2024-01-17 20:42:22 +0530

    status model

---
+++ server/models/statusModel.js
@@ -0,0 +1,36 @@
+class Status extends Model {
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: "user_id" });
+    this.belongsTo(models.Gym, { foreignKey: "gym_id" });
+    this.belongsTo(models.User, { foreignKey: "admin_id" });
+  }
+}
+
+Status.init(
+  {
+    status_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+    },
+    gym_id: {
+      type: DataTypes.INTEGER,
+    },
+    admin_id: {
+      type: DataTypes.INTEGER,
+    },
+    status: DataTypes.ENUM("Active", "Inactive"),
+    role: DataTypes.ENUM("User", "Admin", "Gym"),
+  },
+  {
+    sequelize,
+    modelName: "Status",
+    tableName: "status",
+    timestamps: false,
+  }
+);
+
+module.exports = Status;
