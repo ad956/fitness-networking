0f27311b08e1d153d95787f26a6f825ba0917ab8
Anand Suthar <anandsuthar956@gmail.com>
2024-01-17 20:41:44 +0530

    gym services  model

---
+++ server/models/gymServices.js
@@ -0,0 +1,29 @@
+class Service extends Model {
+  static associate(models) {
+    this.belongsTo(models.Gym, { foreignKey: "gym_id" });
+  }
+}
+
+Service.init(
+  {
+    service_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    gym_id: {
+      type: DataTypes.INTEGER,
+    },
+    service_name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    service_img: DataTypes.STRING,
+  },
+  {
+    sequelize,
+    modelName: "Service",
+    tableName: "service",
+    timestamps: false,
+  }
+);
+
+module.exports = Service;
