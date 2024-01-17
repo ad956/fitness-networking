c29fed40e239e8a4d1f5380840cad8634d6383ee
Anand Suthar <anandsuthar956@gmail.com>
2024-01-17 18:17:45 +0530

    /users a GET request

--- server/routes/api/adminRoute.js
+++ server/routes/api/adminRoute.js
@@ -7,7 +7,7 @@ const validateToken = require("../../middleware/validateTokenHandler");
 router.get("/", validateToken, adminController.getAdmin);
 router.post("/login", adminController.login);
 router.post("/register", adminController.register);
-router.post("/users", validateToken, adminController.getUsers);
+router.get("/users", validateToken, adminController.getUsers);
 router.post("/partners", validateToken, adminController.getPartners);
 
 module.exports = router;
