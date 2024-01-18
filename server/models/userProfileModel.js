5c0748cd27f2c9edec46b566a23187495b287cab
Anand Suthar <anandsuthar956@gmail.com>
2024-01-18 23:15:25 +0530

    minor update

--- server/models/userProfileModel.js
+++ server/models/userProfileModel.js
@@ -1,28 +1,24 @@
-const { Model } = require("sequelize");
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/dbConnection");
 
-class Profile extends Model {
-  static associate(models) {
-    this.belongsTo(models.User, { foreignKey: "user_id" });
-  }
-}
-
-Profile.init(
-  {
-    user_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-    },
-    credit_balance: DataTypes.DECIMAL(10, 2),
-    total_spent: DataTypes.DECIMAL(10, 2),
-    current_gym_name: DataTypes.STRING,
-    requested_gym_name: DataTypes.STRING,
+const Profile = sequelize.define("Profile", {
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
   },
-  {
-    sequelize,
-    modelName: "Profile",
-    tableName: "profile",
-    timestamps: false,
-  }
-);
+  credit_balance: {
+    type: DataTypes.DECIMAL(10, 2),
+  },
+  total_spent: {
+    type: DataTypes.DECIMAL(10, 2),
+  },
+  current_gym_name: {
+    type: DataTypes.STRING(50),
+  },
+  requested_gym_name: {
+    type: DataTypes.STRING(50),
+  },
+});
 
 module.exports = Profile;
