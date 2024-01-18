1184b713b9f6a35c7dad09a1be0a0ad3b66e0270
Anand Suthar <anandsuthar956@gmail.com>
2024-01-18 21:58:57 +0530

    defined transactions model

--- server/models/transactionsModel.js
+++ server/models/transactionsModel.js
@@ -1,38 +1,42 @@
-const { Model } = require("sequelize");
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/dbConnection");
 
-module.exports = (sequelize, DataTypes) => {
-  class Transactions extends Model {
-    static associate(models) {
-      this.belongsTo(models.User, { foreignKey: "user_id" });
-      this.belongsTo(models.Partner, { foreignKey: "gym_id" });
-    }
-  }
+const User = require("./userModel");
+const Gym = require("./partnerModel");
 
-  Transactions.init(
-    {
-      transaction_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-      },
-      gym_id: {
-        type: DataTypes.INTEGER,
-      },
-      transaction_type: DataTypes.ENUM("Purchase", "Usage"),
-      transaction_amount: DataTypes.DECIMAL(5, 2),
-      credit_purchased: DataTypes.DECIMAL(5, 2),
-      transaction_date: DataTypes.DATE,
-    },
-    {
-      sequelize,
-      modelName: "Transactions",
-      tableName: "transactions",
-      timestamps: false,
-    }
-  );
+const Transactions = sequelize.define("Transactions", {
+  transaction_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  gym_id: {
+    type: DataTypes.INTEGER,
+  },
+  transaction_type: {
+    type: DataTypes.ENUM("Purchase", "Usage"),
+    allowNull: false,
+  },
+  transaction_amount: {
+    type: DataTypes.DECIMAL(5, 2),
+    allowNull: false,
+  },
+  credit_purchased: {
+    type: DataTypes.DECIMAL(5, 2),
+    allowNull: false,
+  },
+  transaction_date: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+});
 
-  return Transactions;
-};
+Transactions.belongsTo(User, { foreignKey: "user_id" });
+Transactions.belongsTo(Gym, { foreignKey: "gym_id" });
+
+module.exports = Transactions;
