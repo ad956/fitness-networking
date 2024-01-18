91f9c9337724f747f91c5a4042d12a99fc1c56cc
Anand Suthar <anandsuthar956@gmail.com>
2024-01-18 21:55:08 +0530

    defined user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,51 +1,39 @@
-const { Model } = require("sequelize");
 const Profile = require("./userProfileModel");
 
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    static associate(models) {
-      this.hasOne(models.Profile, { foreignKey: "user_id" });
-      this.belongsToMany(models.Partner, {
-        through: "GymUsers",
-        foreignKey: "user_id",
-      });
-      this.hasMany(models.Status, { foreignKey: "user_id" });
-      this.hasMany(models.Transactions, { foreignKey: "user_id" });
-    }
-  }
-  User.init(
-    {
-      user_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      mobile: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      profile_photo: DataTypes.STRING,
-      otp: DataTypes.STRING,
-    },
-    {
-      sequelize,
-      modelName: "User",
-      tableName: "user",
-      timestamps: false,
-    }
-  );
-  return User;
-};
+const User = sequelize.define("User", {
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING(60),
+    allowNull: false,
+  },
+  mobile: {
+    type: DataTypes.STRING(15),
+    allowNull: false,
+  },
+  profile_photo: {
+    type: DataTypes.STRING(255),
+  },
+  otp: {
+    type: DataTypes.STRING(6),
+  },
+  gender: {
+    type: DataTypes.ENUM("Male", "Female", "Other"),
+    allowNull: false,
+  },
+});
+
+module.exports = User;
