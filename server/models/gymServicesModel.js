cf54697f1dac469ac2ccce0dd323880d2d61ca96
Anand Suthar <anandsuthar956@gmail.com>
2024-01-18 21:52:29 +0530

    defined service model

--- server/models/gymServicesModel.js
+++ server/models/gymServicesModel.js
@@ -1,33 +1,22 @@
 const { Model } = require("sequelize");
 
-module.exports = (sequelize, DataTypes) => {
-  class Service extends Model {
-    static associate(models) {
-      this.belongsTo(models.Partner, { foreignKey: "gym_id" });
-    }
-  }
-
-  Service.init(
-    {
-      service_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      gym_id: {
-        type: DataTypes.INTEGER,
-      },
-      service_name: DataTypes.STRING,
-      description: DataTypes.TEXT,
-      service_img: DataTypes.STRING,
-    },
-    {
-      sequelize,
-      modelName: "Service",
-      tableName: "service",
-      timestamps: false,
-    }
-  );
-
-  return Service;
-};
+const Service = sequelize.define("Service", {
+  service_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  gym_id: {
+    type: DataTypes.INTEGER,
+  },
+  service_name: {
+    type: DataTypes.STRING(50),
+  },
+  description: {
+    type: DataTypes.TEXT,
+  },
+  service_img: {
+    type: DataTypes.STRING(255),
+  },
+});
