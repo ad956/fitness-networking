2d7fa48a11b71de5d6a59afd258d78479282f577
Anand Suthar <anandsuthar956@gmail.com>
2024-01-20 12:08:44 +0530

    updated model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -4,41 +4,48 @@ const Profile = require("./userProfileModel");
 const Partner = require("./partnerModel");
 const GymUsers = require("./gymUsersModel");
 
-const User = sequelize.define("User", {
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+const User = sequelize.define(
+  "User",
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(60),
+      allowNull: false,
+    },
+    mobile: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+    },
+    profile_photo: {
+      type: DataTypes.STRING(255),
+    },
+    otp: {
+      type: DataTypes.STRING(6),
+    },
+    gender: {
+      type: DataTypes.ENUM("Male", "Female", "Other"),
+      allowNull: false,
+    },
   },
-  name: {
-    type: DataTypes.STRING(30),
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING(30),
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING(60),
-    allowNull: false,
-  },
-  mobile: {
-    type: DataTypes.STRING(15),
-    allowNull: false,
-  },
-  profile_photo: {
-    type: DataTypes.STRING(255),
-  },
-  otp: {
-    type: DataTypes.STRING(6),
-  },
-  gender: {
-    type: DataTypes.ENUM("Male", "Female", "Other"),
-    allowNull: false,
-  },
-});
+  {
+    tableName: "user",
+    timestamps: false,
+  }
+);
 
 // circular dependency issues
 User.belongsToMany(Partner, { through: GymUsers, foreignKey: "user_id" });
--- server/models/userProfileModel.js
+++ server/models/userProfileModel.js
@@ -1,24 +1,32 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/dbConnection");
 
-const Profile = sequelize.define("Profile", {
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
+const Profile = sequelize.define(
+  "Profile",
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    credit_balance: {
+      type: DataTypes.DECIMAL(10, 2),
+    },
+    total_spent: {
+      type: DataTypes.DECIMAL(10, 2),
+    },
+    current_gym_name: {
+      type: DataTypes.STRING(50),
+    },
+    requested_gym_name: {
+      type: DataTypes.STRING(50),
+    },
   },
-  credit_balance: {
-    type: DataTypes.DECIMAL(10, 2),
-  },
-  total_spent: {
-    type: DataTypes.DECIMAL(10, 2),
-  },
-  current_gym_name: {
-    type: DataTypes.STRING(50),
-  },
-  requested_gym_name: {
-    type: DataTypes.STRING(50),
-  },
-});
+
+  {
+    tableName: "profile",
+    timestamps: false,
+  }
+);
 
 module.exports = Profile;
