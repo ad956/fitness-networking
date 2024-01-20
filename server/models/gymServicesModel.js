7c3cfdc66917b364bc86575ae102e7a6c6af93b5
Anand Suthar <anandsuthar956@gmail.com>
2024-01-20 12:05:23 +0530

    updated model

--- server/models/gymServicesModel.js
+++ server/models/gymServicesModel.js
@@ -1,25 +1,32 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/dbConnection");
 
-const Service = sequelize.define("Service", {
-  service_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+const Service = sequelize.define(
+  "Service",
+  {
+    service_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    gym_id: {
+      type: DataTypes.INTEGER,
+    },
+    service_name: {
+      type: DataTypes.STRING(50),
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    service_img: {
+      type: DataTypes.STRING(255),
+    },
   },
-  gym_id: {
-    type: DataTypes.INTEGER,
-  },
-  service_name: {
-    type: DataTypes.STRING(50),
-  },
-  description: {
-    type: DataTypes.TEXT,
-  },
-  service_img: {
-    type: DataTypes.STRING(255),
-  },
-});
+  {
+    tableName: "service",
+    timestamps: false,
+  }
+);
 
 module.exports = Service;
