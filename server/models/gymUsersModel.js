7ba6a6d030e160c13bdce9518026d17119c0687a
Anand Suthar <anandsuthar956@gmail.com>
2024-01-20 12:06:08 +0530

    updated model

--- server/models/gymUsersModel.js
+++ server/models/gymUsersModel.js
@@ -1,13 +1,20 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/dbConnection");
 
-const GymUsers = sequelize.define("GymUsers", {
-  gym_id: {
-    type: DataTypes.INTEGER,
+const GymUsers = sequelize.define(
+  "GymUsers",
+  {
+    gym_id: {
+      type: DataTypes.INTEGER,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+    },
   },
-  user_id: {
-    type: DataTypes.INTEGER,
-  },
-});
+  {
+    tableName: "gym_users",
+    timestamps: false,
+  }
+);
 
 module.exports = GymUsers;
