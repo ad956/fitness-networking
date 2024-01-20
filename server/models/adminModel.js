ffa7a61859169222eb4a891f53cc20bfe596ecd9
Anand Suthar <anandsuthar956@gmail.com>
2024-01-20 12:01:35 +0530

    updated model

--- server/models/adminModel.js
+++ server/models/adminModel.js
@@ -2,36 +2,43 @@ const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/dbConnection");
 const Status = require("./statusModel");
 
-const Admin = sequelize.define("Admin", {
-  admin_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+const Admin = sequelize.define(
+  "Admin",
+  {
+    admin_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    mobile: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(60),
+      allowNull: false,
+    },
+    profilephoto: {
+      type: DataTypes.STRING(255),
+    },
+    otp: {
+      type: DataTypes.STRING(6),
+    },
   },
-  email: {
-    type: DataTypes.STRING(50),
-    allowNull: false,
-  },
-  mobile: {
-    type: DataTypes.STRING(15),
-    allowNull: false,
-  },
-  name: {
-    type: DataTypes.STRING(50),
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING(60),
-    allowNull: false,
-  },
-  profilephoto: {
-    type: DataTypes.STRING(255),
-  },
-  otp: {
-    type: DataTypes.STRING(6),
-  },
-});
+  {
+    tableName: "admin",
+    timestamps: false,
+  }
+);
 
 Admin.hasMany(Status, { foreignKey: "admin_id" });
 
