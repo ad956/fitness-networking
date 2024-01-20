0cadb55da6b1711ac977c3ce7eaf0c33d12dab25
Anand Suthar <anandsuthar956@gmail.com>
2024-01-20 12:07:10 +0530

    updated model

--- server/models/statusModel.js
+++ server/models/statusModel.js
@@ -1,30 +1,37 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/dbConnection");
 
-const Status = sequelize.define("Status", {
-  status_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+const Status = sequelize.define(
+  "Status",
+  {
+    status_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+    },
+    gym_id: {
+      type: DataTypes.INTEGER,
+    },
+    admin_id: {
+      type: DataTypes.INTEGER,
+    },
+    status: {
+      type: DataTypes.ENUM("Active", "Inactive"),
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("User", "Admin", "Gym"),
+      allowNull: false,
+    },
   },
-  user_id: {
-    type: DataTypes.INTEGER,
-  },
-  gym_id: {
-    type: DataTypes.INTEGER,
-  },
-  admin_id: {
-    type: DataTypes.INTEGER,
-  },
-  status: {
-    type: DataTypes.ENUM("Active", "Inactive"),
-    allowNull: false,
-  },
-  role: {
-    type: DataTypes.ENUM("User", "Admin", "Gym"),
-    allowNull: false,
-  },
-});
+  {
+    tableName: "status",
+    timestamps: false,
+  }
+);
 
 module.exports = Status;
