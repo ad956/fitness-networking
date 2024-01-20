a2cfea4e00d35dbc94c5a8dddc9160c7a6e4027f
Anand Suthar <anandsuthar956@gmail.com>
2024-01-20 12:04:50 +0530

    updated model

--- server/models/gymProfileModel.js
+++ server/models/gymProfileModel.js
@@ -1,39 +1,46 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/dbConnection");
 
-const GymProfile = sequelize.define("GymProfile", {
-  gym_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
+const GymProfile = sequelize.define(
+  "GymProfile",
+  {
+    gym_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    total_credits: {
+      type: DataTypes.DECIMAL(10, 2),
+    },
+    upi_id: {
+      type: DataTypes.STRING(50),
+    },
+    credit_charge: {
+      type: DataTypes.DECIMAL(5, 2),
+    },
+    todays_credit: {
+      type: DataTypes.DECIMAL(10, 2),
+    },
+    profile_pic: {
+      type: DataTypes.STRING(255),
+    },
+    city: {
+      type: DataTypes.STRING(50),
+    },
+    pin_code: {
+      type: DataTypes.STRING(10),
+    },
+    state: {
+      type: DataTypes.STRING(50),
+    },
+    address: {
+      type: DataTypes.STRING(255),
+    },
   },
-  total_credits: {
-    type: DataTypes.DECIMAL(10, 2),
-  },
-  upi_id: {
-    type: DataTypes.STRING(50),
-  },
-  credit_charge: {
-    type: DataTypes.DECIMAL(5, 2),
-  },
-  todays_credit: {
-    type: DataTypes.DECIMAL(10, 2),
-  },
-  profile_pic: {
-    type: DataTypes.STRING(255),
-  },
-  city: {
-    type: DataTypes.STRING(50),
-  },
-  pin_code: {
-    type: DataTypes.STRING(10),
-  },
-  state: {
-    type: DataTypes.STRING(50),
-  },
-  address: {
-    type: DataTypes.STRING(255),
-  },
-});
+  {
+    tableName: "gym_profile",
+    timestamps: false,
+  }
+);
 
 module.exports = GymProfile;
