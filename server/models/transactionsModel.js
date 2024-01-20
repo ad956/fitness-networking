af9bde96bfdebcfb8f7c9b49093a47bb4f2cd13c
Anand Suthar <anandsuthar956@gmail.com>
2024-01-20 12:07:42 +0530

    updated model

--- server/models/transactionsModel.js
+++ server/models/transactionsModel.js
@@ -4,37 +4,44 @@ const { sequelize } = require("../config/dbConnection");
 const User = require("./userModel");
 const Gym = require("./partnerModel");
 
-const Transactions = sequelize.define("Transactions", {
-  transaction_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+const Transactions = sequelize.define(
+  "Transactions",
+  {
+    transaction_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    gym_id: {
+      type: DataTypes.INTEGER,
+    },
+    transaction_type: {
+      type: DataTypes.ENUM("Purchase", "Usage"),
+      allowNull: false,
+    },
+    transaction_amount: {
+      type: DataTypes.DECIMAL(5, 2),
+      allowNull: false,
+    },
+    credit_purchased: {
+      type: DataTypes.DECIMAL(5, 2),
+      allowNull: false,
+    },
+    transaction_date: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
   },
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  gym_id: {
-    type: DataTypes.INTEGER,
-  },
-  transaction_type: {
-    type: DataTypes.ENUM("Purchase", "Usage"),
-    allowNull: false,
-  },
-  transaction_amount: {
-    type: DataTypes.DECIMAL(5, 2),
-    allowNull: false,
-  },
-  credit_purchased: {
-    type: DataTypes.DECIMAL(5, 2),
-    allowNull: false,
-  },
-  transaction_date: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-});
+  {
+    tableName: "transactions",
+    timestamps: false,
+  }
+);
 
 Transactions.belongsTo(User, { foreignKey: "user_id" });
 Transactions.belongsTo(Gym, { foreignKey: "gym_id" });
