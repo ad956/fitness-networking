bca0cd5135ca09783dcfedfcdc6fce0dde9c69bf
Anand Suthar <anandsuthar956@gmail.com>
2024-01-26 00:00:23 +0530

    minor

--- server/services/userServices.js
+++ server/services/userServices.js
@@ -1,5 +1,8 @@
 const asyncHandler = require("express-async-handler");
 const Transaction = require("../models/transactionsModel");
+const mailTemplateGenrator = require("../services/emailTemplateGenrator");
+const sendEmail = require("../services/sendEmailService");
+const { constants } = require("../utils/constants");
 
 const purchaseCreditsTransaction = asyncHandler(async (transactionData) => {
   const {
@@ -24,8 +27,57 @@ const purchaseCreditsTransaction = asyncHandler(async (transactionData) => {
     credits: creditPointsToBeAdded,
   });
 
+  const introMsg =
+    "Thank you for your recent transaction. This email confirms the success of your transaction.";
+  const instuctMsg = "Details of the transaction:";
+  const link = "";
+  const msg = `<table style='border-collapse: collapse; width: 100%;'>
+  <tr style='border: 1px solid black;'>
+    <td style='border: 1px solid black; padding: 8px;'>Transaction Id</td>
+    <td style='border: 1px solid black; padding: 8px;'>${saveTransaction.toJSON()}</td>
+  </tr>
+  <tr style='border: 1px solid black;'>
+    <td style='border: 1px solid black; padding: 8px;'>Transaction Type</td>
+    <td style='border: 1px solid black; padding: 8px;'>Purchase</td>
+  </tr>
+  <tr style='border: 1px solid black;'>
+    <td style='border: 1px solid black; padding: 8px;'>Transaction Amount</td>
+    <td style='border: 1px solid black; padding: 8px;'>${amountValue}</td>
+  </tr>
+  <tr style='border: 1px solid black;'>
+    <td style='border: 1px solid black; padding: 8px;'>Newly Added</td>
+    <td style='border: 1px solid black; padding: 8px;'>${updatedCreditPoints}</td>
+  </tr>
+  <tr style='border: 1px solid black;'>
+    <td style='border: 1px solid black; padding: 8px;'>Total Credits</td>
+    <td style='border: 1px solid black; padding: 8px;'>${updatedCreditPoints}</td>
+  </tr>
+</table>
+`;
+  const outro =
+    "If you have any questions or concerns about this transaction, please contact our customer support.";
+
+  let mail = mailTemplateGenrator(
+    user.name,
+    introMsg,
+    instuctMsg,
+    link,
+    msg,
+    outro
+  );
+
+  // sending an email ...
+  let message = {
+    from: constants.MAIL_FROM,
+    to: user.email,
+    subject: "Recent Transaction",
+    html: mail,
+  };
+
+  const info = await sendEmail(message);
+
   // Return success indicator
-  return saveTransaction;
+  return info;
 });
 
 module.exports = { purchaseCreditsTransaction };
