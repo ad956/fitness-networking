f1bb1472b21ecc7f08dc57fd55ca7049a03adf9c
Anand Suthar <anandsuthar956@gmail.com>
2024-03-24 23:54:38 +0530

    minor

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "@fontsource/lato";
 import "./index.css";
-import { ThemeProvider } from "@mui/material";
 import { BrowserRouter as Router } from "react-router-dom";
-import { theme } from "./theme/theme.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
+      <App />
     </Router>
   </React.StrictMode>
 );
