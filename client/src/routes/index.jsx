958bec82d8895cc60da34ea162b7bd6598fb4591
Anand Suthar <anandsuthar956@gmail.com>
2024-03-24 23:28:06 +0530

    import alias changes

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -3,15 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import UserRoutes from "./userRoutes";
 import PartnerRoutes from "./partnerRoutes";
 import AdminRoutes from "./adminRoutes";
-import Login from "../features/auth/Login/";
-import Signup from "../features/auth/Signup";
-import ErrorPage from "../pages/errorPage";
+import Login from "@pages/auth/Login/";
+import Signup from "@pages/auth/Signup";
+import { ErrorPage } from "@components";
 
 export default function AppRoutes() {
   return (
     <Routes>
       {/* <Route path="/" element={<>Hey Boii</>} /> */}
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<UserRoutes />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/user/*" element={<UserRoutes />} />
