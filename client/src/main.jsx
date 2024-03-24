dcc20b46adb7b39241ddc5707e7714fb8a69b915
Anand Suthar <anandsuthar956@gmail.com>
2024-03-25 00:18:19 +0530

    Added NextUI provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { NextUIProvider } from "@nextui-org/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
     </Router>
   </React.StrictMode>
 );
