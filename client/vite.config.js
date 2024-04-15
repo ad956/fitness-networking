8d9f2ca3caa8a2de1ece43919ecd36826ece9c6b
Anand Suthar <anandsuthar956@gmail.com>
2024-04-15 16:14:56 +0530

    Added new alias

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,9 @@ export default defineConfig({
   resolve: {
     alias: {
       "@app": "/src/app",
+      "@api": "/src/api",
       "@assets": "/src/assets",
+      "@images": "/src/assets/images",
       "@components": "/src/components",
       "@features": "/src/features",
       "@pages": "/src/pages",
