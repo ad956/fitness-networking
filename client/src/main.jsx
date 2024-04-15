1adaa64a0bfe3b1d59382f81a8cae7630c1df5fe
Anand Suthar <anandsuthar956@gmail.com>
2024-04-15 16:15:57 +0530

    Added ErrorBoundary, Helmet & React Query Proivders

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,23 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { NextUIProvider } from "@nextui-org/react";
+import { HelmetProvider } from "react-helmet-async";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ErrorBoundary, ErrorFallback } from "@components";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <NextUIProvider>
-        <App />
-      </NextUIProvider>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <HelmetProvider>
+          <QueryClientProvider client={queryClient}>
+            <NextUIProvider>
+              <App />
+            </NextUIProvider>
+          </QueryClientProvider>
+        </HelmetProvider>
+      </ErrorBoundary>
     </Router>
   </React.StrictMode>
 );
