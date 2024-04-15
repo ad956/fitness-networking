4aedd5270eccf042f9853dc2522ff59809bba96b
Anand Suthar <anandsuthar956@gmail.com>
2024-04-15 18:58:54 +0530

    minor configurations

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,7 +8,11 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: "Outfit",
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
