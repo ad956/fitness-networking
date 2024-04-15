55aa4b1befd4c18e51ddcd2db11af3e5cf99cbfd
Anand Suthar <anandsuthar956@gmail.com>
2024-04-15 23:58:29 +0530

    Added server url constant

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 const SERVER_URL =
-  import.meta.env.REACT_APP_SERVER_URL || "http://localhost:3000/";
+  import.meta.env.REACT_APP_SERVER_URL || "http://localhost:3000/api/";
 
 export { SERVER_URL };
