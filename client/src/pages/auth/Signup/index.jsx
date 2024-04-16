7e4221a61cc3a80d5dbaafe855bf27e4d60c911e
Anand Suthar <anandsuthar956@gmail.com>
2024-04-16 23:17:14 +0530

    Completed signup-page with firebase google-auth & email-password

--- client/src/pages/auth/Signup/index.jsx
+++ client/src/pages/auth/Signup/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Image,
   Button,
@@ -9,20 +9,22 @@ import {
 } from "@nextui-org/react";
 import { AiTwotoneEye, AiTwotoneEyeInvisible } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
-import { signin_png } from "@images";
-import { SeoHelmet } from "@components";
-import { loginUser } from "@api";
+import { signup_jpg } from "@images";
+import { SeoHelmet, GoogleAuthHandler } from "@components";
+import { signupUser } from "@api";
 import toast, { Toaster } from "react-hot-toast";
 import { useMutation } from "@tanstack/react-query";
 
 function SignupPage() {
-  const [user, setUser] = React.useState({
-    identifier: "",
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    mobile: "",
     password: "",
     role: "member",
   });
 
-  const [isVisible, setIsVisible] = React.useState(false);
+  const [isVisible, setIsVisible] = useState(false);
   const toggleVisibility = () => setIsVisible(!isVisible);
 
   const handleChange = (e) => {
@@ -46,24 +48,24 @@ function SignupPage() {
   };
 
   const { mutate, isLoading, isError, error, data } = useMutation({
-    mutationFn: loginUser,
+    mutationFn: signupUser,
   });
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
-    if (!user.identifier || !user.password) {
+    if (!user.name || !user.email || !user.mobile || !user.password) {
       toast.error("Please fill in all fields.");
       return;
     }
-    if (user.identifier.includes("@")) {
-      if (!validateEmail(user.identifier)) {
+    if (user.email.includes("@")) {
+      if (!validateEmail(user.email)) {
         toast.error("Please enter a valid email address.");
         return;
       }
     }
-    if (!user.identifier.includes("@")) {
-      if (!validateMobileNumber(user.identifier)) {
+    if (!user.email.includes("@")) {
+      if (!validateMobileNumber(user.email)) {
         toast.error(
           "Mobile number must be a valid format (e.g. +1234567890 or 123-456-7890)"
         );
@@ -90,18 +92,26 @@ function SignupPage() {
     }
   };
 
-  const title = "Login Page";
+  const handleGoogleSignInButton = async () => {
+    try {
+      await GoogleAuthHandler();
+    } catch (error) {
+      console.error(error);
+      toast.error(error);
+    }
+  };
+
+  const title = "Sign Up | Fitness Networking";
   const canonical = window.location.href;
 
   return (
     <section className="bg-white/75 min-h-screen min-w-screen flex justify-around items-center px-4 sm:px-6 lg:px-8">
       <SeoHelmet title={title} canonical={canonical} />
       <Toaster />
-      <Image src={signin_png} className="hidden md:block h-3/5 w-4/5" />
-      <div className="max-w-md w-full space-y-8 lg:mr-10">
+      <div className="w-full lg:w-2/6 space-y-5">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-zinc-900">
-            Sign in to your account
+            Sign up for your account
           </h2>
         </div>
 
@@ -110,37 +120,80 @@ function SignupPage() {
             <Button
               variant="bordered"
               radius="full"
+              onClick={handleGoogleSignInButton}
               className="w-full flex justify-center items-center p-6 text-sm font-medium"
             >
               <FcGoogle size={25} />
-              Sign in with Google
+              Sign up with Google
             </Button>
           </div>
           <div className="relative flex py-5 items-center">
             <div className="flex-grow border-t border-zinc-300 -700"></div>
             <span className="flex-shrink mx-4 text-zinc-600">
-              or sign in with email or username
+              or sign up with email & mobile
             </span>
             <div className="flex-grow border-t border-zinc-300"></div>
           </div>
 
           <form onSubmit={handleFormSubmit}>
-            <div>
+            <div className="flex flex-row">
               <Input
-                id="identifier"
-                name="identifier"
+                id="name"
+                name="name"
                 type="text"
-                autoComplete="email mobile"
-                label="Email / Mobile"
+                autoComplete="name"
+                label="Name"
                 labelPlacement="outside"
                 required
-                value={user.identifier}
+                value={user.name}
                 onChange={handleChange}
                 className="w-full px-3 py-2"
                 classNames={{
                   label: "text-black font-medium",
                 }}
-                placeholder="Enter email or mobile"
+                placeholder="Enter name"
+                variant="bordered"
+                size="lg"
+                radius="md"
+              />
+              <Input
+                id="mobile"
+                name="mobile"
+                type="text"
+                autoComplete="mobile"
+                label="Mobile"
+                labelPlacement="outside"
+                required
+                maxLength={10}
+                startContent={<span className="text-black/80">+91</span>}
+                value={user.mobile}
+                onChange={handleChange}
+                className="w-full px-3 py-2"
+                classNames={{
+                  label: "text-black font-medium",
+                }}
+                placeholder="Enter mobile"
+                variant="bordered"
+                size="lg"
+                radius="md"
+              />
+            </div>
+            <div>
+              <Input
+                id="email"
+                name="email"
+                type="text"
+                autoComplete="email"
+                label="Email"
+                labelPlacement="outside"
+                required
+                value={user.email}
+                onChange={handleChange}
+                className="w-full px-3 py-2"
+                classNames={{
+                  label: "text-black font-medium",
+                }}
+                placeholder="Enter email"
                 variant="bordered"
                 size="lg"
                 radius="md"
@@ -151,7 +204,7 @@ function SignupPage() {
                 id="password"
                 name="password"
                 type={isVisible ? "text" : "password"}
-                autoComplete="current-password"
+                autoComplete="new-password"
                 label="Password"
                 labelPlacement="outside"
                 required
@@ -161,7 +214,7 @@ function SignupPage() {
                 classNames={{
                   label: "text-black font-medium",
                 }}
-                placeholder="********"
+                placeholder="Enter password"
                 variant="bordered"
                 size="lg"
                 radius="md"
@@ -206,19 +259,22 @@ function SignupPage() {
               className="w-full bg-black text-white tracking-wide font-medium"
               radius="md"
             >
-              Sign in
+              Sign up
             </Button>
           </form>
         </div>
 
         <div className="text-center">
           <p className="text-sm font-medium text-zinc-600">
-            Don't have an account?{" "}
+            Already have an account?{" "}
             <Link href="#" className="text-sm text-black/80">
-              Sign up
+              Sign in
             </Link>
           </p>
         </div>
+      </div>
+      <div className="hidden min-h-screen w-3/6  lg:flex items-center">
+        <Image src={signup_jpg} className="min-h-full min-w-full border-3" />
       </div>
     </section>
   );
