068769045d7b38ba6ca4e4443a13a358b818c239
Anand Suthar <anandsuthar956@gmail.com>
2024-04-16 23:20:59 +0530

    Added google-auth route for users

--- server/routes/api/partnerRoute.js
+++ server/routes/api/partnerRoute.js
@@ -4,9 +4,17 @@ const router = express.Router();
 const partnerController = require("../../controllers/partnerController");
 const validateToken = require("../../middleware/validateTokenHandler");
 const validatePasswordToken = require("../../middleware/PasswordTokenHandler");
+const verifyGoogleIdToken = require("../../middleware/verifyGoogleIdToken");
+// const googleAuth = require("./api/googleAuth");
 
 router.get("/", validateToken, partnerController.getPartner);
 router.post("/login", partnerController.login);
+router.get(
+  "/login/:token",
+  validatePasswordToken,
+  partnerController.redirectUser
+);
+router.post("/google-auth", verifyGoogleIdToken);
 router.post("/register", partnerController.register);
 router.post("/forgot-password", partnerController.forgetPassword); // at login
 router.post("/reset-password", validateToken, partnerController.resetPassword); // using profile
--- server/routes/api/userRoute.js
+++ server/routes/api/userRoute.js
@@ -4,11 +4,14 @@ const router = express.Router();
 const userController = require("../../controllers/userController");
 const validateToken = require("../../middleware/validateTokenHandler");
 const validatePasswordToken = require("../../middleware/PasswordTokenHandler");
+const verifyGoogleIdToken = require("../../middleware/verifyGoogleIdToken");
+// const googleAuth = require("./api/googleAuth");
 
 router.get("/", validateToken, userController.getUser);
 router.get("/all", validateToken, userController.allUsers);
 router.post("/login", userController.login);
-router.get("/login/:token", validatePasswordToken);
+router.get("/login/:token", validatePasswordToken, userController.redirectUser);
+router.post("/google-auth", verifyGoogleIdToken, userController.googleAuth);
 router.post("/register", userController.registerUser);
 router.post("/forgot-password", userController.forgetPassword); // at login
 router.post("/reset-password", validateToken, userController.resetPassword); // using profile
