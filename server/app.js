246a5a69089a5310a2386e4e2c4e4729b8a9aba0
Anand Suthar <anandsuthar956@gmail.com>
2024-04-17 18:09:25 +0530

    Added socket.IO

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
+const http = require("http"); // Import the http module
 const cors = require("cors");
 const port = process.env.PORT || 3000;
 
@@ -8,14 +8,24 @@ const initialRoute = require("./routes");
 const errorHandler = require("./middleware/errorHandler");
 const { connectDB } = require("./config/dbConnection");
 
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
 app.use(cors());
 connectDB();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+app.set("io", io);
 app.use("/api", initialRoute);
 app.use(errorHandler);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
