8b200ec2683da54883364c3ece602e1a544b8497
Anand Suthar <anandsuthar956@gmail.com>
2024-04-17 18:06:50 +0530

    minor change from REACT_APP to VITE_APP

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 const SERVER_URL =
-  import.meta.env.REACT_APP_SERVER_URL || "http://localhost:3000/api/";
+  import.meta.env.VITE_APP_SERVER_URL || "http://localhost:3000/api/";
 
 export { SERVER_URL };
