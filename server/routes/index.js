96ebb47e08d2a1301eeb3fe968b93f8b24a9ba5f
Anand Suthar <anandsuthar956@gmail.com>
2024-04-18 18:32:50 +0530

    added comment spceifying user and thier respective routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,12 @@ const userRoutes = require("./api/userRoute");
 const partnerRoutes = require("./api/partnerRoute");
 const adminRoutes = require("./api/adminRoute");
 
+/*
+  - '/user'   : for gym members
+  - '/partner': for gym owners
+  - '/admin'  : for administrative tasks
+*/
+
 router.use("/user", userRoutes);
 router.use("/partner", partnerRoutes);
 router.use("/admin", adminRoutes);
