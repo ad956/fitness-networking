f1668524e40f5d9b2fd504393ce9c1893fd4644a
Anand Suthar <anandsuthar956@gmail.com>
2024-04-20 21:16:37 +0530

    minor

--- client/src/api/auth/googleAuthToken.jsx
+++ client/src/api/auth/googleAuthToken.jsx
@@ -6,22 +6,13 @@ export default async function googleAuthToken(token, userRole) {
     Authorization: `Bearer ${token}`,
   };
 
-  try {
-    const response = await axios.post(
-      `${SERVER_URL}${userRole}/google-auth`,
-      null,
-      {
-        headers,
-      }
-    );
-
-    if (response.message) {
-      throw new Error("Google Authentication failed");
+  const response = await axios.post(
+    `${SERVER_URL}${userRole}/google-auth`,
+    null,
+    {
+      headers,
     }
+  );
 
-    const res = response.data;
-    return res;
-  } catch (error) {
-    throw new Error("Google Authentication failed. Please try again later.");
-  }
+  return response.data;
 }
