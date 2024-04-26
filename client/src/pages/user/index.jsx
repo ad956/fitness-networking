8addcae2c7a990a74bd8b639b79a7c8b802b5c8c
Anand Suthar <anandsuthar956@gmail.com>
2024-04-26 23:59:31 +0530

    UserLayout with user routes

--- client/src/pages/user/index.jsx
+++ client/src/pages/user/index.jsx
@@ -1,12 +1,17 @@
 // import UserRoutes from "@routes/userRoutes";
 // Sidebar, Headbar
 import { Layout } from "@components";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./components/dashboard";
 
 export default function UserLayout() {
   return (
     <Layout>
-      <section className="h-screen w-full flex flex-row border-2 border-black">
-        APPLICATION
+      <section className="h-screen w-full flex flex-row border2 border-black">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/qrcode" element={<p>Profile</p>} />
+        </Routes>
       </section>
     </Layout>
   );
