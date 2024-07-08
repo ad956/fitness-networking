5d3848488dbe9fb89d649823c435fd30b200ef41
Anand Suthar <anandsuthar956@gmail.com>
2024-07-08 23:48:07 +0530

    refactor : added index.js for components

---
+++ client/src/pages/user/components/index.js
@@ -0,0 +1,7 @@
+import Dashboard from "./dashboard";
+import Membership from "./membership";
+import Profile from "./profile";
+import QRCode from "./qr-code";
+import Transactions from "./transactions";
+
+export { Dashboard, Membership, Profile, QRCode, Transactions };
