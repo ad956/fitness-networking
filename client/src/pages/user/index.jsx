90b28a5672ee4ff7ba1c9f8857ec4ac57f9fbdc9
Anand Suthar <anandsuthar956@gmail.com>
2024-07-08 23:49:45 +0530

    updated routes

--- client/src/pages/user/index.jsx
+++ client/src/pages/user/index.jsx
@@ -2,15 +2,18 @@
 // Sidebar, Headbar
 import { Layout } from "@components";
 import { Routes, Route } from "react-router-dom";
-import Dashboard from "./components/dashboard";
+import { Dashboard, Membership, Transactions, Profile } from "./components/";
 
 export default function UserLayout() {
   return (
     <Layout>
-      <section className="h-screen w-full flex flex-row border2 border-black">
+      <section className="h-screen w-full flex flex-row overflow-y-scroll">
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/qrcode" element={<p>Profile</p>} />
+          <Route path="/membership" element={<Membership />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </section>
     </Layout>
