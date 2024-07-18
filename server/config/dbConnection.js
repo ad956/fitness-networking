891b82f7ea9b83c6af318d34bd791a2319c545a7
Anand Suthar <anandsuthar956@gmail.com>
2024-07-18 17:48:39 +0530

    ✨ Enhance database connection configuration with production support
    - Added conditional logic to handle different configurations for production and development environments.
    - Implemented SSL options for secure connections in production.

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -1,19 +1,34 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST || "host.docker.internal",
-    dialect: "mysql",
-  }
-);
+let sequelize;
+
+const isProduction = process.env.NODE_ENV === "production";
+
+if (isProduction && process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      dialect: "mysql",
+    }
+  );
+}
 
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
+    console.log("Database connection established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
