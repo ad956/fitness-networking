c9f14d8224701c6f3111ae8f9b893c0678eea692
Anand Suthar <anandsuthar956@gmail.com>
2024-07-18 16:38:22 +0530

    fix : deployment issue with passwordTokenHandler module
    - Updated code to correctly reference passwordTokenHandler.js file
    - Resolves Cannot find module ../../middleware/passwordTokenHandler' error

---
+++ server/middleware/passwordTokenHandler.js
@@ -0,0 +1,37 @@
+const asyncHandler = require("express-async-handler");
+const { constants } = require("../utils/constants");
+const User = require("../models/userModel");
+const Partner = require("../models/partnerModel");
+
+/**
+ * Middleware to validate the one-time password (OTP) token and attach the user/partner to the request for further processing.
+ */
+
+const validatePasswordToken = asyncHandler(async (req, res, next) => {
+  const { token } = req.params;
+
+  const user = req.originalUrl.includes("user")
+    ? await User.findOne({ where: { otp: token } })
+    : await Partner.findOne({ where: { otp: token } });
+
+  if (!user) {
+    res
+      .status(401)
+      .redirect(
+        `${constants.CLIENT_ERROR_URL}?msg=RESET%20TOKEN%20IS%20NOT%20VALID%20OR%20EXPIRED`
+      );
+
+    return;
+  }
+
+  user.otp = null;
+  const result = await user.save();
+  if (!result) {
+    res.status(500);
+    throw new Error("Internal Server Error");
+  }
+  req.user = user;
+  next();
+});
+
+module.exports = validatePasswordToken;
