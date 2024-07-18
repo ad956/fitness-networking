00e32dad22904faf95047ebe377676e09379c1e8
Anand Suthar <anandsuthar956@gmail.com>
2024-07-18 23:18:50 +0530

    Hard-code appID

--- client/src/components/Firebase/index.js
+++ client/src/components/Firebase/index.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  appId: "fitness-networking", // import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
