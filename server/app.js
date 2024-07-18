43e1673092a2346bc31f259773c926fc34c397aa
Anand Suthar <anandsuthar956@gmail.com>
2024-07-18 17:06:26 +0530

    Add conditional loading of dotenv for non-production environments

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const http = require("http"); // Import the http module
 const cors = require("cors");
