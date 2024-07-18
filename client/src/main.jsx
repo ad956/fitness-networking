98913d45fb33f337fb64062b148397dcf27278df
Anand Suthar <anandsuthar956@gmail.com>
2024-07-18 22:58:13 +0530

    Remove Initialize Firebase

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,9 +10,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ErrorBoundary, ErrorFallback } from "@components";
 const queryClient = new QueryClient();
 
-// initialize Firebase
-import "./components/Firebase";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ReduxProvider store={store}>
