fb72ccc3bd5bcf1e2893b4b42c8f7200e95cf9bd
Anand Suthar <anandsuthar956@gmail.com>
2024-07-18 22:22:20 +0530

    Remove loadEnv

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
   return {
     resolve: {
       alias: {
@@ -35,30 +33,5 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [react()],
     base: "./",
-    define: {
-      "import.meta.env.VITE_BASE_URL": JSON.stringify(env.VITE_BASE_URL),
-      "import.meta.env.VITE_SERVER_URL": JSON.stringify(env.VITE_SERVER_URL),
-      "import.meta.env.VITE_FIREBASE_API_KEY": JSON.stringify(
-        env.VITE_FIREBASE_API_KEY
-      ),
-      "import.meta.env.VITE_FIREBASE_AUTH_DOMAIN": JSON.stringify(
-        env.VITE_FIREBASE_AUTH_DOMAIN
-      ),
-      "import.meta.env.VITE_FIREBASE_PROJECT_ID": JSON.stringify(
-        env.VITE_FIREBASE_PROJECT_ID
-      ),
-      "import.meta.env.VITE_FIREBASE_STORAGE_BUCKET": JSON.stringify(
-        env.VITE_FIREBASE_STORAGE_BUCKET
-      ),
-      "import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
-        env.VITE_FIREBASE_MESSAGING_SENDER_ID
-      ),
-      "import.meta.env.VITE_FIREBASE_APP_ID": JSON.stringify(
-        env.VITE_FIREBASE_APP_ID
-      ),
-      "import.meta.env.VITE_FIREBASE_MEASUREMENT_ID": JSON.stringify(
-        env.VITE_FIREBASE_MEASUREMENT_ID
-      ),
-    },
   };
 });
