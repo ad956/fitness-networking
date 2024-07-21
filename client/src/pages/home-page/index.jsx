d0e9f504a6a884e0df915a48939dd77fdbe93a34
Anand Suthar <anandsuthar956@gmail.com>
2024-07-21 18:30:32 +0530

    Add process.env.VITE_SERVER_URL

--- client/src/pages/home-page/index.jsx
+++ client/src/pages/home-page/index.jsx
@@ -63,6 +63,7 @@ export default function HomePage() {
   console.log("========== PRINTING ALL ENVS =============");
   console.log(import.meta.env);
   console.log(process.env);
+  console.log("server : " + process.env.VITE_SERVER_URL);
 
   return (
     <div className="font-Inter h-full w-full">
