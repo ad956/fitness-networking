e2081463bbba1f5c5d314ab02151df61ccf5d56a
Anand Suthar <anandsuthar956@gmail.com>
2024-07-21 19:03:13 +0530

    Define envs in vite config

--- client/src/pages/home-page/index.jsx
+++ client/src/pages/home-page/index.jsx
@@ -62,6 +62,7 @@ export default function HomePage() {
 
   console.log("========== PRINTING ALL ENVS =============");
   console.log(import.meta.env);
+  console.log("=<" + process.env.VITE_SERVER_URL);
 
   return (
     <div className="font-Inter h-full w-full">
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
   return {
+    define: {
+      "process.env": env,
+    },
     resolve: {
       alias: {
         "@app": "/src/app",
