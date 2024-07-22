58c00471ce0831d45add39c915aa06bce5099b1e
Anand Suthar <anandsuthar956@gmail.com>
2024-07-22 21:44:40 +0530

    Remove loadEnv

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
   return {
-    define: {
-      "process.env": env,
-    },
     resolve: {
       alias: {
         "@app": "/src/app",
