1c05b76463b5e70347dc8802961864dec028cb47
Anand Suthar <anandsuthar956@gmail.com>
2024-07-31 00:01:32 +0530

    refactor : Update index.js

--- client/src/pages/user/components/index.js
+++ client/src/pages/user/components/index.js
@@ -1,7 +1,8 @@
 import Dashboard from "./dashboard";
+import Headbar from "./headbar";
 import Membership from "./membership";
 import Profile from "./profile";
 import QRCode from "./qr-code";
 import Transactions from "./transactions";
 
-export { Dashboard, Membership, Profile, QRCode, Transactions };
+export { Dashboard, Headbar, Membership, Profile, QRCode, Transactions };
