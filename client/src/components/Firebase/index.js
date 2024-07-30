4e3338c0c7e7ac614992e10b1f73f16369f1d6e0
Anand Suthar <anandsuthar956@gmail.com>
2024-07-30 23:59:16 +0530

    refactor : Remove unnecessary process.env

--- client/src/components/Firebase/index.js
+++ client/src/components/Firebase/index.js
@@ -3,25 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey:
-    process.env.VITE_FIREBASE_API_KEY || import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain:
-    process.env.VITE_FIREBASE_AUTH_DOMAIN ||
-    import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId:
-    process.env.VITE_FIREBASE_PROJECT_ID ||
-    import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket:
-    process.env.VITE_FIREBASE_STORAGE_BUCKET ||
-    import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:
-    process.env.VITE_FIREBASE_MESSAGING_SENDER_ID ||
-    import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId:
-    process.env.VITE_FIREBASE_APP_ID || import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId:
-    process.env.VITE_FIREBASE_MEASUREMENT_ID ||
-    import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
