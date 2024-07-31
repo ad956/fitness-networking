8bb1e265bd2ee53975a512176f2892429f437e14
Anand Suthar <anandsuthar956@gmail.com>
2024-07-31 18:41:04 +0530

    refactor: Remove SocketIO and its components

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require("express");
-const http = require("http"); // Import the http module
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 3000;
@@ -13,17 +12,6 @@ const errorHandler = require("./middleware/errorHandler");
 const { connectDB } = require("./config/dbConnection");
 
 const app = express();
-const server = http.createServer(app);
-const io = require("socket.io")(server, {
-  cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://fitness-networking.onrender.com",
-    ],
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
 
 app.use(
   cors({
@@ -39,11 +27,10 @@ connectDB();
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.set("io", io);
 app.use("/api", initialRoute);
 
 app.use(errorHandler);
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
