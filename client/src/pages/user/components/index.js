fe7b5a76c710950fd73a5c2225a4c87ff88a0f65
Anand Suthar <anandsuthar956@gmail.com>
2024-07-31 18:55:18 +0530

    refactor: Remove components folder

--- client/src/pages/user/components/index.js
+++
@@ -1,8 +0,0 @@
-import Dashboard from "./Dashboard";
-import Headbar from "./Headbar";
-import Membership from "./Membership";
-import Profile from "./Profile";
-import QRCode from "./QRCode";
-import Transactions from "./Transactions";
-
-export { Dashboard, Headbar, Membership, Profile, QRCode, Transactions };
