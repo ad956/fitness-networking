9446bb637b5becb967a7495ac586a25cd33d9c96
Anand Suthar <anandsuthar956@gmail.com>
2024-08-09 23:41:30 +0530

    fix (queries): Update useMutation to have mutationFn

--- client/src/api/queries/authQueries/index.jsx
+++ client/src/api/queries/authQueries/index.jsx
@@ -5,15 +5,14 @@ import googleAuthToken from "./googleAuthToken";
 import loginUser from "./loginUser";
 import signupUser from "./signupUser";
 
-export { googleAuthToken, loginUser, signupUser };
+export const useLogin = () => useMutation({ mutationFn: loginUser });
 
-export const useLogin = () => useMutation(loginUser);
+export const useGoogleAuth = () => useMutation({ mutationFn: googleAuthToken });
 
-export const useGoogleAuth = () => useMutation(googleAuthToken);
+export const useSignup = () => useMutation({ mutationFn: signupUser });
 
-export const useSignup = () => useMutation(signupUser);
-
-export const useLogout = () => useMutation(() => axios.post("/logout"));
+export const useLogout = () =>
+  useMutation({ mutationFn: () => axios.post("/logout") });
 
 export const useCheckAuth = () =>
   useQuery("auth", () => axios.get("/check-auth"), {
