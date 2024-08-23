2107def383947d2476532b7ef0085fdad00ced65
Anand Suthar <anandsuthar956@gmail.com>
2024-08-23 19:36:04 +0530

    refactor : Export PublicRoute

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,12 +4,13 @@ import Footer from "./Footer";
 import Sidebar from "./Sidebar";
 import SeoHelmet from "./SeoHelmet";
 import PageNotFound from "./PageNotFound";
+import ProtectedRoute from "./ProtectedRoute";
+import PublicRoute from "./PublicRoute";
 import ErrorBoundary from "./ErrorBoundary";
 import ErrorFallback from "./ErrorFallback";
 import AuthErrorFallback from "./AuthErrorFallback";
 import GoogleAuthHandler from "./Firebase/GoogleAuthHandler";
 import VerificationModal from "./VerificationModal";
-import ProtectedRoute from "./ProtectedRoute";
 
 export {
   Layout,
@@ -18,10 +19,11 @@ export {
   Footer,
   SeoHelmet,
   PageNotFound,
+  ProtectedRoute,
+  PublicRoute,
   ErrorBoundary,
   ErrorFallback,
   AuthErrorFallback,
   GoogleAuthHandler,
   VerificationModal,
-  ProtectedRoute,
 };
