ed8234bea3020c15adcac19d5e9ff9f935843c9c
Anand Suthar <anandsuthar956@gmail.com>
2024-08-23 19:41:06 +0530

    removed unnecessary routes

--- client/src/routes/adminRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { Routes, Route } from "react-router-dom";
-
-const AdminRoutes = () => (
-  <Routes>
-    <Route path="/" element={<p>Index page of ADMIN</p>} />
-  </Routes>
-);
-
-export default AdminRoutes;
--- client/src/routes/partnerRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { Routes, Route } from "react-router-dom";
-
-const PartnerRoutes = () => (
-  <Routes>
-    <Route path="/" element={<p>Index page of Partner</p>} />
-  </Routes>
-);
-
-export default PartnerRoutes;
