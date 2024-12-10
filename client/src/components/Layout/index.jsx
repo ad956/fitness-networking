1dca0ae1bdfadbab32679dfb36e02fc920951de7
Anand Suthar <anandsuthar956@gmail.com>
2024-12-10 18:57:42 +0530

    🎨 refactor: enhance main content scrolling behavior 📜✨
    - Added overflow-y-scroll to main content area
    - Isolated scrolling to main section, preventing sidebar scroll
    - Improved user experience by localizing scrollable region

--- client/src/components/Layout/index.jsx
+++ client/src/components/Layout/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Sidebar from "../Sidebar";
 import Headbar from "../Headbar";
-import { Card } from "@nextui-org/react";
 
 const Layout = ({ children }) => {
   return (
-    <div className="font-inter h-screen w-screen flex flex-row justify-between items-center">
+    <div className="font-inter h-screen w-screen flex flex-row justify-between items-center overflow-hidden">
       <Sidebar />
-      <main className="h-screen w-full flex flex-col">{children}</main>
+      <main className="h-screen w-full flex flex-col overflow-y-scroll">
+        {children}
+      </main>
     </div>
   );
 };
