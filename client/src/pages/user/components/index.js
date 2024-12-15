98cfb75a3cce46b57ccacb8606cf27e013ceeaec
Anand Suthar <anandsuthar956@gmail.com>
2024-12-15 18:09:36 +0530

    chore: remove GymAvailability and UpcomingClasses components

--- client/src/pages/user/components/index.js
+++ client/src/pages/user/components/index.js
@@ -1,18 +1,7 @@
 import ProfileSettings from "./Profile";
 import QRCode from "./QRCode";
-
 import CreditStats from "./CreditStats";
-import GymAvailability from "./GymAvailability";
 import GymMap from "./GymMap";
-import UpcomingClasses from "./UpcomingClasses";
 import WorkoutStats from "./WorkoutStats";
 
-export {
-  ProfileSettings,
-  QRCode,
-  CreditStats,
-  GymAvailability,
-  GymMap,
-  UpcomingClasses,
-  WorkoutStats,
-};
+export { ProfileSettings, QRCode, CreditStats, GymMap, WorkoutStats };
