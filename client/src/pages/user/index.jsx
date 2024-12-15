4443cb5ee081e9a04622cd0bef9608f2dcab715b
Anand Suthar <anandsuthar956@gmail.com>
2024-12-15 18:10:01 +0530

    chore: remove GymAvailability and UpcomingClasses components

--- client/src/pages/user/index.jsx
+++ client/src/pages/user/index.jsx
@@ -1,11 +1,5 @@
 import { Layout } from "@components";
-import {
-  CreditStats,
-  GymAvailability,
-  GymMap,
-  UpcomingClasses,
-  WorkoutStats,
-} from "./components/";
+import { CreditStats, GymMap, WorkoutStats } from "./components/";
 
 export default function UserLayout() {
   return (
@@ -14,10 +8,6 @@ export default function UserLayout() {
         <div className="p-8">
           <CreditStats purchased={100} spent={45} remaining={55} />
           <WorkoutStats />
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mb-6">
-            <GymAvailability currentUsers={15} maxCapacity={20} />
-            <UpcomingClasses />
-          </div>
           <GymMap />
         </div>
       </div>
