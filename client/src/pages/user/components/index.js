634d481c5c0bb26e1aa1814ba87775f31c7cfaa8
Anand Suthar <anandsuthar956@gmail.com>
2024-12-15 17:53:30 +0530

    refactor : remove Transaction and Membership exports

--- client/src/pages/user/components/index.js
+++ client/src/pages/user/components/index.js
@@ -1,8 +1,6 @@
 import Headbar from "./Headbar";
-import Membership from "./Membership";
 import ProfileSettings from "./Profile";
 import QRCode from "./QRCode";
-import Transactions from "./Transactions";
 
 import CreditStats from "./CreditStats";
 import GymAvailability from "./GymAvailability";
@@ -12,10 +10,8 @@ import WorkoutStats from "./WorkoutStats";
 
 export {
   Headbar,
-  Membership,
   ProfileSettings,
   QRCode,
-  Transactions,
   CreditStats,
   GymAvailability,
   GymMap,
