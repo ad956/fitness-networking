ec92e7b262f0aea0c77a0f93dd94f3ebecdd121c
Anand Suthar <anandsuthar956@gmail.com>
2024-12-15 18:03:49 +0530

    feat: Add headbar component

--- client/src/components/Layout/index.jsx
+++ client/src/components/Layout/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import Sidebar from "../Sidebar";
 import Headbar from "../Headbar";
+import Sidebar from "../Sidebar";
 
 const Layout = ({ children }) => {
   return (
     <div className="font-inter h-screen w-screen flex flex-row justify-between items-center overflow-hidden">
       <Sidebar />
-      <main className="h-screen w-full flex flex-col overflow-y-scroll">
+      <main className="h-screen w-full flex flex-col">
+        <Headbar />
         {children}
       </main>
     </div>
