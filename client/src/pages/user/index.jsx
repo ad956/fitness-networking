716c21b95796b10203458b782e17949e5e9a7a61
Anand Suthar <anandsuthar956@gmail.com>
2024-12-25 17:35:40 +0530

    chore: 🧹 rename Membership component to MembershipPlans

--- client/src/pages/user/index.jsx
+++ client/src/pages/user/index.jsx
@@ -2,7 +2,7 @@ import { Layout } from "@components";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import QRCodePage from "./QRCode";
-import Membership from "./Membership";
+import MembershipPlans from "./MembershipPlans";
 import Transactions from "./Transactions";
 import Profile from "./Profile";
 import { PageNotFound } from "@components";
@@ -14,7 +14,7 @@ export default function UserLayout() {
         <Route index element={<Navigate to="/user/dashboard" replace />} />
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="qr-code" element={<QRCodePage />} />
-        <Route path="membership" element={<Membership />} />
+        <Route path="membership" element={<MembershipPlans />} />
         <Route path="transactions" element={<Transactions />} />
         <Route path="profile" element={<Profile />} />
         {/* <Route path="*" element={<PageNotFound />} /> */}
