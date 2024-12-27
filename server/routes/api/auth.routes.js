5c91a5b79f5bde3faab7c0cec91b1c4f401ebafb
Anand Suthar <anandsuthar956@gmail.com>
2024-12-27 15:31:50 +0530

    feat: 🛠️ add authentication routes for login, logout, register, and password management

---
+++ server/routes/api/auth.routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const authController = require("../../controllers/auth.controller");
+const verifyGoogleIdToken = require("../../middleware/verify-google-id-token.middleware");
+
+router.post("/login", authController.login);
+router.post("/logout", authController.logout);
+router.post("/register", authController.register);
+router.post("/google-auth", verifyGoogleIdToken, authController.googleAuth);
+router.post("/forgot-password", authController.forgotPassword);
+router.get("/verify/:token", authController.verifyUser);
+router.get("/validate-login/:identifier", authController.validateLogin);
+
+module.exports = router;
