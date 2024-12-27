efa325db4b1d8084fa1f434bbf73c5986bce1fa4
Anand Suthar <anandsuthar956@gmail.com>
2024-12-27 15:37:20 +0530

    refactor: 🧹 renamed files to kebab-case

--- server/services/otpGenration.js
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto");
-
-const buffer = crypto.randomBytes(5);
-const genratedOTP = buffer
-  .toString("base64")
-  .replace(/[+/=]/g, "")
-  .substring(0, 5);
-
-module.exports = genratedOTP;
