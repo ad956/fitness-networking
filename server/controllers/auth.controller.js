9caffddfdfa3f0ddaa00c4f35673095f9562b5e5
Anand Suthar <anandsuthar956@gmail.com>
2024-12-27 16:44:02 +0530

    feat(auth): refactor AuthService and AuthController for better handling and flow 🛠️

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,74 +1,54 @@
 const authService = require("../services/auth.service");
 const asyncHandler = require("express-async-handler");
-const { constants } = require("../utils");
+const { constants, templates } = require("../utils");
 
 class AuthController {
   constructor(authService) {
     this.authService = authService;
-    this.cookieOptions = {
-      maxAge: constants.COOKIE_MAX_AGE_MS,
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-    };
-  }
-
-  validateRequestBody(fields, body) {
-    const missingFields = fields.filter((field) => !body[field]);
-    if (missingFields.length > 0) {
-      throw new Error(`Missing required fields: ${missingFields.join(", ")}`);
-    }
-  }
-
-  setCookies(res, { accessToken, refreshToken }) {
-    res.cookie("refreshToken", refreshToken, this.cookieOptions);
-    res.cookie("accessToken", accessToken, this.cookieOptions);
-  }
-
-  clearCookies(res) {
-    res.clearCookie("accessToken");
-    res.clearCookie("refreshToken");
   }
 
   register = asyncHandler(async (req, res) => {
-    this.validateRequestBody(["name", "email", "password", "mobile"], req.body);
-
     const { name, email, password, mobile } = req.body;
-    const userType = req.path.split("/")[1]; // Extracts user type from URL
 
-    const newUser = await this.authService.register(
-      {
-        name,
-        email,
-        password,
-        mobile,
-      },
-      userType
-    );
+    if (!name || !email || !password || !mobile) {
+      res.status(400);
+      throw new Error("All fields are mandatory");
+    }
 
+    const newUser = await this.authService.register({
+      name,
+      email,
+      password,
+      mobile,
+    });
     res.status(201).json({ newUser });
   });
 
   login = asyncHandler(async (req, res) => {
-    this.validateRequestBody(["identifier", "password"], req.body);
-
     const { identifier, password } = req.body;
-    const userType = req.path.split("/")[1];
 
-    await this.authService.login(identifier, password, userType);
+    if (!identifier || !password) {
+      res.status(400);
+      throw new Error("All fields are mandatory");
+    }
+
+    await this.authService.login(identifier, password);
     res.status(200).json({ msg: "verification pending" });
   });
 
   logout = asyncHandler(async (req, res) => {
-    this.clearCookies(res);
+    const refreshToken = req.cookies.refreshToken;
+    await this.authService.logout(refreshToken);
+
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
     res.status(200).json({ message: "Logged out successfully" });
   });
 
   verifyUser = asyncHandler(async (req, res) => {
     const { token } = req.params;
-    const userType = req.path.split("/")[1];
+    const user = await this.authService.verifyUserToken(token);
 
-    const user = await this.authService.verifyUserToken(token, userType);
     if (!user) {
       return res
         .status(401)
@@ -77,21 +57,70 @@ class AuthController {
         );
     }
 
-    const tokens = this.authService.generateAuthTokens(user.id, userType);
-    this.setCookies(res, tokens);
+    const { accessToken, refreshToken } = this.authService.generateAuthTokens(
+      user.id
+    );
 
-    res.status(200).send(templates.verifiedUserTemplate);
+    res
+      .cookie("refreshToken", refreshToken, {
+        maxAge: constants.COOKIE_MAX_AGE_MS,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+      })
+      .cookie("accessToken", accessToken, {
+        maxAge: constants.COOKIE_MAX_AGE_MS,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+      })
+      .status(200)
+      .send(templates.verifiedUserTemplate);
+  });
+
+  validateLogin = asyncHandler(async (req, res) => {
+    const { identifier } = req.params;
+
+    if (!identifier) {
+      res.status(400);
+      throw new Error("Identifier field is mandatory");
+    }
+
+    const result = await this.authService.validateLogin(identifier);
+    res.status(200).json(result);
   });
 
   googleAuth = asyncHandler(async (req, res) => {
-    const { email } = req;
-    const userType = req.path.split("/")[1];
+    const email = req.email; // Set by verifyGoogleIdToken middleware
 
-    const user = await this.authService.googleAuth(email, userType);
-    const tokens = this.authService.generateAuthTokens(user.user_id, userType);
+    const user = await this.authService.googleAuth(email);
+    const { accessToken, refreshToken } = this.authService.generateAuthTokens(
+      user.user_id
+    );
 
-    this.setCookies(res, tokens);
-    res.status(200).json({ accessToken: tokens.accessToken });
+    res
+      .cookie("refreshToken", refreshToken, {
+        maxAge: constants.COOKIE_MAX_AGE_MS,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+      })
+      .status(200)
+      .json({ accessToken });
+  });
+
+  forgotPassword = asyncHandler(async (req, res) => {
+    const { email_mobile } = req.body;
+
+    if (!email_mobile) {
+      res.status(400);
+      throw new Error("Email or Mobile is mandatory");
+    }
+
+    await this.authService.forgotPassword(email_mobile);
+    res
+      .status(200)
+      .json({ message: "Password reset instructions sent to your email" });
   });
 }
 
--- server/services/auth.service.js
+++ server/services/auth.service.js
@@ -1,31 +1,29 @@
 const bcrypt = require("bcrypt");
 const { Op } = require("sequelize");
+
+const Admin = require("../models/admin.modal");
+const Partner = require("../models/partner.modal");
+const User = require("../models/user.modal");
+
 const { sendEmail, templateGenrator } = require("../services/email/");
 const { constants, genratedOTP, tokens } = require("../utils/");
 
 class AuthService {
   constructor() {
-    this.models = {
-      admin: require("../models/admin.modal"),
-      partner: require("../models/partner.modal"),
-      user: require("../models/user.modal"),
-    };
-
-    this.bcryptRounds = parseInt(process.env.BCRYPT_ROUNDS, 10) || 10;
-    this.emailService = sendEmail;
-    this.templateGenerator = templateGenrator;
+    this.getModel = this.getModel.bind(this);
   }
 
   getModel(userType) {
-    const model = this.models[userType.toLowerCase()];
-    if (!model) {
-      throw new Error("Invalid user type");
+    switch (userType.toLowerCase()) {
+      case "admin":
+        return Admin;
+      case "partner":
+        return Partner;
+      case "user":
+        return User;
+      default:
+        throw new Error("Invalid user type");
     }
-    return model;
-  }
-
-  async hashPassword(password) {
-    return await bcrypt.hash(password, this.bcryptRounds);
   }
 
   async register(userData, userType) {
@@ -40,7 +38,8 @@ class AuthService {
       throw new Error(`${userType} already exists`);
     }
 
-    const hashedPassword = await this.hashPassword(password);
+    const bcryptRounds = parseInt(process.env.BCRYPT_ROUNDS, 10) || 10;
+    const hashedPassword = await bcrypt.hash(password, bcryptRounds);
 
     return Model.create({
       name,
@@ -50,15 +49,9 @@ class AuthService {
     });
   }
 
-  async validateCredentials(user, password) {
-    if (!user) {
-      return false;
-    }
-    return await bcrypt.compare(password, user.password);
-  }
-
   async login(identifier, password, userType) {
     const Model = this.getModel(userType);
+
     const user = await Model.findOne({
       where: { [Op.or]: [{ email: identifier }, { mobile: identifier }] },
     });
@@ -67,12 +60,12 @@ class AuthService {
       throw new Error(`${userType} doesn't exist`);
     }
 
-    const isPasswordValid = await this.validateCredentials(user, password);
+    const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
       throw new Error("Invalid credentials");
     }
 
-    const verificationSecret = genratedOTP();
+    const verificationSecret = generateOTP;
     user.otp = verificationSecret;
     await user.save();
 
@@ -95,10 +88,62 @@ class AuthService {
     return user;
   }
 
+  async validateLogin(identifier, userType) {
+    const Model = this.getModel(userType);
+    const user = await Model.findOne({
+      where: { [Op.or]: [{ email: identifier }, { mobile: identifier }] },
+    });
+
+    if (!user) {
+      throw new Error(`${userType} doesn't exist`);
+    }
+
+    return { verified: user.otp === null };
+  }
+
+  async googleAuth(email, userType) {
+    const Model = this.getModel(userType);
+    const user = await Model.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new Error(`${userType} not found. Please sign up.`);
+    }
+
+    return user;
+  }
+
+  async forgotPassword(identifier, userType) {
+    const Model = this.getModel(userType);
+    const user = await Model.findOne({
+      where: { [Op.or]: [{ email: identifier }, { mobile: identifier }] },
+    });
+
+    if (!user) {
+      throw new Error(`${userType} doesn't exist`);
+    }
+
+    const forgotPasswordSecret = genratedOTP;
+    user.otp = forgotPasswordSecret;
+    await user.save();
+
+    await this.sendForgotPasswordEmail(user, forgotPasswordSecret, userType);
+
+    return true;
+  }
+
+  generateAuthTokens(userId, userType) {
+    return {
+      accessToken: tokens.generateAccessToken(userId, userType),
+      refreshToken: tokens.generateRefreshToken(userId, userType),
+    };
+  }
+
   async sendLoginVerificationEmail(user, verificationSecret, userType) {
     const verificationLink = `${constants.USER_URL}${userType}/verify/${verificationSecret}`;
 
-    const mail = this.templateGenerator(
+    const mail = templateGenrator(
       user.name,
       `You have received this email because a login request for your ${userType} account was received.`,
       "Click the button below to login to your account:",
@@ -115,15 +160,31 @@ class AuthService {
       html: mail,
     };
 
-    return this.emailService(message);
+    return sendEmail(message);
   }
 
-  generateAuthTokens(userId, userType) {
-    return {
-      accessToken: tokens.generateAccessToken(userId, userType),
-      refreshToken: tokens.generateRefreshToken(userId, userType),
+  async sendForgotPasswordEmail(user, forgotPasswordSecret, userType) {
+    const forgotPasswordLink = `${constants.USER_URL}${userType}/reset-password/${forgotPasswordSecret}`;
+
+    const mail = templateGenrator(
+      user.name,
+      `You have received this email because a forgot password request for your ${userType} account was received.`,
+      "Click the button below to reset your password:",
+      forgotPasswordLink,
+      "Reset your password",
+      "If you did not request a password reset, no further action is required on your part.",
+      "#3457dc"
+    );
+
+    const message = {
+      from: constants.MAIL_FROM,
+      to: user.email,
+      subject: `Reset Password for ${userType} Account`,
+      html: mail,
     };
+
+    return sendEmail(message);
   }
 }
 
-module.exports = new AuthService();
+module.exports = AuthService;
