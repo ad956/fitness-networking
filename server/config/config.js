b840a2f053ebcf3922c327ad5cddd4cdb22b7557
Anand Suthar <anandsuthar956@gmail.com>
2024-12-31 22:22:55 +0530

    fix(config): update database configuration to use environment variables
    - Added support for environment variables (DB_USER, DB_PASSWORD, DB_NAME, DB_HOST) for development and test environments.
    - Set production to use DB_URL from environment variables for a more secure connection.
    - Configured SSL options for the production environment.

--- server/config/config.js
+++ server/config/config.js
@@ -1,23 +1,20 @@
 module.exports = {
   development: {
-    username: "root",
-    password: "1245",
-    database: "fitness",
-    host: "127.0.0.1",
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "1245",
+    database: process.env.DB_NAME || "fitness",
+    host: process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql",
   },
   test: {
-    username: "root",
-    password: "null",
-    database: "database_test",
-    host: "127.0.0.1",
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "1245",
+    database: process.env.DB_NAME || "fitness",
+    host: process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql",
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    use_env_variable: process.env.DB_URL,
     dialect: "mysql",
     dialectOptions: {
       ssl: {
