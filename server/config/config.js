0e3a682e3d27a4cfcba27ecc4b8fba1333991c2d
Anand Suthar <anandsuthar956@gmail.com>
2024-12-31 22:10:27 +0530

    chore: add config.js for database configuration

---
+++ server/config/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  development: {
+    username: "root",
+    password: "1245",
+    database: "fitness",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: "null",
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
--- server/config/db.config.js
+++ server/config/db.config.js
@@ -15,11 +15,11 @@ if (isProduction && process.env.DB_URL) {
   });
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
+    process.env.DB_NAME || "fitness",
+    process.env.DB_USER || "root",
+    process.env.DB_PASS || "1245",
     {
-      host: process.env.DB_HOST,
+      host: process.env.DB_HOST || "127.0.0.1",
       dialect: "mysql",
     }
   );
