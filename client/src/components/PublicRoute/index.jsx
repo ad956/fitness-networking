54d4f5b96cdb53ab3d5ff35f2c871eaab9873abe
Anand Suthar <anandsuthar956@gmail.com>
2024-12-31 18:37:51 +0530

    refactor: remove isLoading

--- client/src/components/PublicRoute/index.jsx
+++ client/src/components/PublicRoute/index.jsx
@@ -1,18 +1,9 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { SpinnerLoader } from "@components";
 import { useCheckAuth } from "@hooks";
 
 const PublicRoute = ({ children }) => {
-  const { data: authData, isLoading } = useCheckAuth();
-
-  if (isLoading) {
-    return (
-      <div className="h-screen grid place-items-center">
-        <SpinnerLoader />
-      </div>
-    );
-  }
+  const { data: authData } = useCheckAuth();
 
   if (authData?.isAuthenticated) {
     return <Navigate to={`/${authData.role}`} replace />;
